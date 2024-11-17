ff169537314508761a3ec081f5e63646626aecc3
prampokan <prampokan@gmail.com>
2024-11-17 21:02:22 +0700

    react pdf canvas to preview CV

--- app/(components)/navbar.tsx
+++ app/(components)/navbar.tsx
@@ -1,7 +1,10 @@
+import { SidebarTrigger } from "@/components/ui/sidebar"
+
 export default function Navbar() {
     return (
-        <nav className="w-full flex justify-center items-center h-14 border-b bg-white fixed z-50">
+        <nav className="w-full flex justify-center items-center h-14 border-b bg-white fixed">
             <div className="w-[70rem]">
+                {/* <SidebarTrigger /> */}
                 <h1 className="font-semibold tracking-tight text-zinc-800">LOGO</h1>
             </div>
         </nav>
--- app/user/layout.tsx
+++ app/user/layout.tsx
@@ -1,11 +1,13 @@
 import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar"
 import { AppSidebar } from "../(components)/sidebar"
+import Navbar from "../(components)/navbar"
 
 export default function UserLayout({ children }: { children: React.ReactNode }) {
   return (
     <SidebarProvider>
+      {/* <Navbar /> */}
       <AppSidebar />
-      <main className="p-5">
+      <main className="p-5 w-full">
         <SidebarTrigger />
         {children}
       </main>
