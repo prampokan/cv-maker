372b05e3cb1aaaf98622b0c997b63f723c3a7478
prampokan <prampokan@gmail.com>
2024-11-29 15:48:34 +0700

    update

--- lib/firebase/init.ts
+++ lib/firebase/init.ts
@@ -4,12 +4,12 @@ import { getStorage } from "firebase/storage";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB6npa3r8KY708PmFTrpiqdN0_lK0uOrhU",
-  authDomain: "cvmakerindo.firebaseapp.com",
-  projectId: "cvmakerindo",
-  storageBucket: "cvmakerindo.firebasestorage.app",
-  messagingSenderId: "795594439664",
-  appId: "1:795594439664:web:74b0e8295ae13817d5bb35",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
