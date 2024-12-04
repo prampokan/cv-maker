d420147a38d75ee4e012e5971c9342c02a908492
prampokan <prampokan@gmail.com>
2024-12-04 23:13:30 +0700

    fix

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,7 @@ import Hero from "./(landingPages)/hero";
 import { Reviews } from "./(landingPages)/reviews";
 import ChooseTemplates from "./(landingPages)/chooseTemplates";
 import Steps from "./(landingPages)/steps";
-import WhyChooseUs from "./(landingPages)/whyChooseUs";
 import Footer from "./(landingPages)/footer";
-import Video from "./(landingPages)/video";
 
 export default function Home() {
   return (
